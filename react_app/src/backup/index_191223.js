dfe1629868803c8121d8a8adadba3773600b0a23
uni51 <uni51@users.noreply.github.com>
2019-12-23 12:01:58 +0900

    バックアップ用ディレクトリにファイルを移動：3-2 プロジェクトでコンポーネント開発

---
+++ react_app/src/backup/index_191223.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import MemoStore from './memo/Store';
+
+// 表示をレンダリング
+ReactDOM.render(
+    <Provider store={MemoStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
---
+++ react_app/src/backup/index_3_12.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+    <App title="App" message="This is App Component!" />,
+    document.getElementById('root')
+);
